1b48ef0f64a9cd84123daae56929e7175d46bef7
TungLeThanh <lethanhtunglc@gmail.com>
2019-03-20 22:51:07 +0700

    dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    // HeaderComponent
   ],
   imports: [
     RouterModule.forRoot([
